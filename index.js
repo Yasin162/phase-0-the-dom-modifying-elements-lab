0775b40cef472f922b638bbd63e6ad7878548c41
Yasin <yasinabdussaburr@gmail.com>
2021-06-02 17:05:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Yasin is the champion'
